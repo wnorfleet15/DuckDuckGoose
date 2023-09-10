e20741272a68b62a8feaf6feeead95ff4981e817
wnorfleet15 <wnorfleet15@outlook.com>
2023-09-10 18:34:09 -0500

    Initial commit

---
+++ ddg.js
@@ -0,0 +1,8 @@
+let randomNumber = Math.floor(Math.random() * 10);
+
+while (randomNumber != 7) {
+  console.log("Duck 🦆");
+  randomNumber = Math.floor(Math.random() * 10);
+}
+
+console.log("Goose! 🪿");
